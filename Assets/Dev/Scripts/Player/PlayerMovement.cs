d29ef99d6f12546d65ebbb709105892d49886152
Naufal Hilmi <naufalhilmi25@gmail.com>
2022-01-04 08:49:37 +0700

    fix bug movement player

--- Assets/Dev/Scripts/Player/PlayerMovement.cs
+++ Assets/Dev/Scripts/Player/PlayerMovement.cs
@@ -54,25 +54,28 @@ public class PlayerMovement : MonoBehaviour
     // Read movement input and set move direction
     public void OnMovementInput(InputAction.CallbackContext inputVal)
     {
-        if (inputVal.performed)
-            movementInput = inputVal.ReadValue<Vector2>();
-            moveDirection = new Vector3(0, 0) { x = movementInput.x, z = movementInput.y };
-            //Debug.Log("[PLAYER] Movement direction: " + moveDirection);
+        movementInput = inputVal.ReadValue<Vector2>();
+        moveDirection = new Vector3(0, 0) { x = movementInput.x, z = movementInput.y };
+        //Debug.Log("[PLAYER] Movement direction: " + moveDirection);
     }
 
     //TODO Sprint with cooldown?
     public void SprintPressed(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
+        {
             isSprinting = true;
             //Debug.Log(this.name + " started sprinting " + isSprinting);
+        }
     }
 
     public void SprintReleased(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
+        {
             isSprinting = false;
             //Debug.Log(this.name + " no longer sprinting " + isSprinting);
+        }
     }
     #endregion
 
