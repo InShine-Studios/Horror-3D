453cfc8a4a0d596a67682bf2df03158b328643d2
Naufal Hilmi <naufalhilmi25@gmail.com>
2022-01-16 00:04:52 +0700

    changing audioManager

--- Assets/Dev/Scripts/Audio/AudioManager.cs
+++ Assets/Dev/Scripts/Audio/AudioManager.cs
@@ -8,32 +8,14 @@ using System;
  */
 public class AudioManager : MonoBehaviour
 {
-
-    #region Variables
-    [Tooltip("List of Playable Sounds")]
-    public Sound[] sounds;
-    #endregion
-
-    void Awake()
-    {
-        foreach (Sound sound in sounds)
-        {
-            sound.source = gameObject.AddComponent<AudioSource>();
-            sound.source.clip = sound.clip;
-            sound.source.volume = sound.volume;
-            sound.source.pitch = sound.pitch;
-            sound.source.loop = sound.loop;
-        }
-    }
-
     public void Play(string name)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        GameObject s = GameObject.Find(name);
         if (s == null)
         {
             Debug.LogWarning("Sound: " + name + " not found!");
             return;
         }
-        s.source.Play();
+        s.GetComponent<AudioSource>().Play();
     }
 }
