f4504e9ae7e60e77db17cafc3031a41117fc1039
Naufal Hilmi <naufalhilmi25@gmail.com>
2022-01-24 13:36:04 +0700

    changing fog color

--- Assets/Astralization/Scripts/Interactables/Item/DummyAnkh.cs
+++ Assets/Astralization/Scripts/Interactables/Item/DummyAnkh.cs
@@ -13,11 +13,19 @@ public class DummyAnkh : Item
     [SerializeField]
     private GameObject _astralWorld;
 
+    private bool _inAstral = false;
+
+    private Color _col;
+
     #endregion
 
     public override void Use()
     {
         //Debug.Log("[ITEM] Use " + this.name);
-        _astralWorld.SetActive(!_astralWorld.activeInHierarchy);
+        bool bConverted = ColorUtility.TryParseHtmlString("#5F466A", out _col);
+        if (!_inAstral & bConverted) RenderSettings.fogColor = _col;
+        else RenderSettings.fogColor = Color.black;
+        _astralWorld.SetActive(!_inAstral);
+        _inAstral = !_inAstral;
     }
 }
