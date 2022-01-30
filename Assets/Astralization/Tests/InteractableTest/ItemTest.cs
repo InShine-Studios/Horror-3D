444d9ed869b7fd1bfbc067d07e1676af55fa5695
Rafif Taris <51603162+rafiftaris@users.noreply.github.com>
2022-01-30 22:46:46 +0700

    [refactor] tests (#134)
    * Refactor tests
    
    * revert changes on inventory
    
    * Adapt astral test to current refactor
    
    * Refactor directory structure
    
    * Adapt test from observer patterns and refactor directory structure
    
    * Refactor door anim
    
    * Fix scroll test
    
    * Delete scene

---
+++ Assets/Astralization/Tests/InteractableTest/ItemTest.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+public class ItemTest : TestBase
+{
+    #region Setup Teardown
+    [SetUp]
+    public override void SetUp()
+    {
+        sceneName = "InteractableTestScene";
+        base.SetUp();
+    }
+    #endregion
+
+    #region Item and Inventory
+    [UnityTest]
+    public IEnumerator PlayerItemDetector_PlayerInventory_PickUseDiscardDummyFlashlight()
+    {
+        yield return new WaitWhile(() => sceneLoaded == false);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.PickItem);
+        GameObject flashlight = player.transform.Find("Rotate/InteractZone/DummyFlashlight").gameObject;
+        Assert.NotNull(flashlight);
+
+        IInventory inventory = player.transform.Find("Rotate/InteractZone").GetComponent<IInventory>();
+        Assert.AreEqual(1, inventory.GetNumOfItem());
+        Assert.NotNull(inventory.GetActiveItem());
+        Assert.AreEqual(0, inventory.GetActiveIdx());
+
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.UseItem);
+        Assert.IsTrue(flashlight.GetComponentInChildren<Light>().enabled);
+        Image img = hud.transform.Find("ItemHud/Logo").GetComponent<Image>();
+        Assert.IsTrue(img.enabled);
+        Assert.AreEqual(flashlight.name, img.sprite.name);
+
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.DiscardItem);
+        GameObject overworldFlashlight = GameObject.Find("OverworldItems/DummyFlashlight");
+        Assert.NotNull(overworldFlashlight);
+        Assert.IsFalse(img.enabled);
+        Assert.AreEqual(0, inventory.GetNumOfItem());
+        Assert.IsNull(inventory.GetActiveItem());
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerItemDetector_PlayerInventory_PickAndUseAnkh()
+    {
+        yield return new WaitWhile(() => sceneLoaded == false);
+        GameObject ankhOW = GameObject.Find("OverworldItems/Ankh");
+        inputTestFixture.Press(KeyboardMouseTestFixture.RegisteredInput.MoveLeft);
+        float moveDuration = GetMovementDurationTowards(ankhOW.transform);
+        yield return new WaitForSeconds(moveDuration);
+        inputTestFixture.Release(KeyboardMouseTestFixture.RegisteredInput.MoveLeft);
+
+        inputTestFixture.Press(KeyboardMouseTestFixture.RegisteredInput.PickItem);
+        yield return null;
+        GameObject ankh = player.transform.Find("Rotate/InteractZone/Ankh").gameObject;
+        Assert.NotNull(ankh);
+
+        IInventory inventory = player.transform.Find("Rotate/InteractZone").GetComponent<IInventory>();
+        Assert.AreEqual(1, inventory.GetNumOfItem());
+        Assert.NotNull(inventory.GetActiveItem());
+        Assert.AreEqual(0, inventory.GetActiveIdx());
+
+        inputTestFixture.Press(KeyboardMouseTestFixture.RegisteredInput.UseItem);
+        yield return null;
+        GameObject astral = GameObject.Find("VOL_Global_AstralWorld_1");
+        Assert.IsTrue(astral.activeInHierarchy);
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerInventory_ScrollActiveItem()
+    {
+        yield return new WaitWhile(() => sceneLoaded == false);
+        IInventory inventory = player.transform.Find("Rotate/InteractZone").GetComponent<IInventory>();
+        int idxBefore = inventory.GetActiveIdx();
+        inputTestFixture.Set("Scroll/Y", inventory.GetScrollStep());
+        yield return null;
+
+        Assert.AreEqual(Utils.MathCalcu.mod(idxBefore - 1, inventory.GetLength()), inventory.GetActiveIdx());
+        Assert.AreEqual(inventory.GetItemByIndex(inventory.GetActiveIdx()), inventory.GetActiveItem());
+        Image img = hud.transform.Find("ItemHud/Logo").GetComponent<Image>();
+        Assert.IsTrue(!img.enabled);
+    }
+    #endregion
+}
