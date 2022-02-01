e6ad7258515aee9adf724afd4c8a20021782e43f
Sedoboi <vadim.hantono@yahoo.co.id>
2022-02-01 20:31:45 +0700

    Fixed some bugs and edits

--- Assets/Astralization/Gameplay/Scripts/States/AstralMeterLogic.cs
+++ Assets/Astralization/Gameplay/Scripts/States/AstralMeterLogic.cs
@@ -22,20 +22,10 @@ public class AstralMeterLogic : MonoBehaviour, IAstralMeterLogic
 
     void Start()
     {
-        InvokeRepeating("Increment", 3.0f, 1.0f);
+        InvokeRepeating("Increment", 0.0f, 1.0f);
     }
 
-    void Increment()
-    {
-        float currentMeter = 0.0f;
-        if (_isOnSight)
-        {
-            currentMeter += _sightAmount;
-        }
-        currentMeter += _constantRate;
-        _astralMeter = System.Math.Min(_maxMeter, _astralMeter + (currentMeter * Time.deltaTime));
-    }
-
+    #region Enable - Disable
     private void OnEnable()
     {
         GameManager.ChangeWorldEvent += ChangeWorld;
@@ -45,26 +35,9 @@ public class AstralMeterLogic : MonoBehaviour, IAstralMeterLogic
     {
         GameManager.ChangeWorldEvent -= ChangeWorld;
     }
+    #endregion
 
-    private void ChangeWorld(bool state)
-    {
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            GameObject go = transform.GetChild(i).gameObject;
-            go.SetActive(state);
-        }
-        ToggleWorldRate(state);
-    }
-
-    private void ToggleWorldRate(bool state)
-    {
-        if (state)
-        {
-            _constantRate = 0.083f;
-        }
-        else _constantRate = 0.05f;
-    }
-
+    #region Getter
     public float GetAstralMeter()
     {
         return _astralMeter;
@@ -79,16 +52,39 @@ public class AstralMeterLogic : MonoBehaviour, IAstralMeterLogic
     {
         return _isOnSight;
     }
+    #endregion
 
+    void Increment()
+    {
+        float currentMeter = 0.0f;
+        if (_isOnSight)
+        {
+            currentMeter += _sightAmount;
+        }
+        currentMeter += _constantRate;
+        _astralMeter = System.Math.Min(_maxMeter, _astralMeter + currentMeter);
+    }
+
+    #region World State
+    private void ChangeWorld(bool state)
+    {
+        ToggleWorldRate(state);
+    }
+
+    private void ToggleWorldRate(bool state)
+    {
+        if (state)
+        {
+            _constantRate = 0.083f;
+        }
+        else _constantRate = 0.05f;
+    }
+    #endregion
+
+    #region Ghost
     public void ChangeSightState()
     {
         _isOnSight = !_isOnSight;
-    }
-
-    public void NPCWrongAnswer()
-    {
-        int randAnswerAmount = Random.Range(10, 15);
-        _astralMeter += (float)randAnswerAmount;
     }
 
     public void PlayerKilled()
@@ -96,4 +92,13 @@ public class AstralMeterLogic : MonoBehaviour, IAstralMeterLogic
         int randKillAmount = Random.Range(15, 20);
         _astralMeter += (float)randKillAmount;
     }
+    #endregion
+
+    #region NPC
+    public void NPCWrongAnswer()
+    {
+        int randAnswerAmount = Random.Range(10, 15);
+        _astralMeter += (float)randAnswerAmount;
+    }
+    #endregion
 }
