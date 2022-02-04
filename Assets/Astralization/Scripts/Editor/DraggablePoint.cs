a189ddf1decd608d5deffbf174a75a56f5ed3faf
Rafif Taris <rafiftaris1@gmail.com>
2022-02-04 11:53:51 +0800

    WIP: roaming ghost

---
+++ Assets/Astralization/Scripts/Editor/DraggablePoint.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+using System.Reflection;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+///Handles relative to game object
+public class DraggablePoint : PropertyAttribute { }
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(StageController), true)]
+public class DraggablePointDrawer : Editor
+{
+    SerializedProperty _roomCoordinates;
+    readonly GUIStyle style = new GUIStyle();
+
+    void OnEnable()
+    {
+        style.fontStyle = FontStyle.Bold;
+        style.normal.textColor = Color.white;
+        _roomCoordinates = serializedObject.FindProperty("Room Coordinates");
+    }
+
+    public void OnSceneGUI()
+    {
+        for (int x = 0; x < _roomCoordinates.arraySize; x++)
+        {
+            SerializedProperty element = _roomCoordinates.GetArrayElementAtIndex(x);
+            SerializedProperty _roomCoord = element.FindPropertyRelative("coordinate");
+            handleVectorPropertyInArray(_roomCoord, _roomCoordinates, x);
+        }
+    }
+
+    void handleVectorPropertyInArray(SerializedProperty property, SerializedProperty parent, int index)
+    {
+        FieldInfo parentfield = serializedObject.targetObject.GetType().GetField(parent.name);
+        if (parentfield == null)
+        {
+            return;
+        }
+        var draggablePoints = parentfield.GetCustomAttributes(typeof(DraggablePoint), false);
+        if (draggablePoints.Length > 0)
+        {
+            Handles.Label(property.vector3Value + ((MonoBehaviour)target).transform.position, parent.name + "[" + index + "]");
+            property.vector3Value = Handles.PositionHandle(property.vector3Value + ((MonoBehaviour)target).transform.position, Quaternion.identity) - ((MonoBehaviour)target).transform.position;
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+#endif
---
+++ Assets/Astralization/Scripts/Stage/StageController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct RoomCoordinate
+{
+    public string name;
+    public Vector3 coordinate;
+}
+
+/**
+ * StageController
+ * Manages stage related behavior.
+ * For example manage room coordinates, etc.
+ */
+public class StageController : MonoBehaviour
+{
+    public List<RoomCoordinate> RoomCoordinates;
+    private List<string> _roomNames;
+    private Dictionary<string, Vector3> _roomCoordDict = new Dictionary<string, Vector3>();
+
+    private void Awake()
+    {
+        foreach (RoomCoordinate roomCoordinate in RoomCoordinates)
+        {
+            _roomCoordDict[roomCoordinate.name] = roomCoordinate.coordinate;
+        }
+        _roomNames = new List<string>(_roomCoordDict.Keys);
+    }
+
+    public Vector3 GetRoomCoordinate(string roomName)
+    {
+        return _roomCoordDict[roomName];
+    }
+
+    public Vector3 GetRandomRoomCoordinate()
+    {
+        int randomIdx = Utils.Randomizer.Rand.Next(RoomCoordinates.Count);
+        string randomKey = _roomNames[randomIdx];
+        return GetRoomCoordinate(randomKey);
+    }
+}
