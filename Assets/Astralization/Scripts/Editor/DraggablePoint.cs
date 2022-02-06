0bdad3cda4320fe97df9671883281dc71473ca20
uci <sef.gustingurah.yama@gmail.com>
2022-02-06 14:43:45 +0800

    add all room coordinate

--- Assets/Astralization/Scripts/Editor/DraggablePoint.cs
+++ Assets/Astralization/Scripts/Editor/DraggablePoint.cs
@@ -23,51 +23,51 @@ public class DraggablePointDrawer : Editor
         _roomCoordinates = serializedObject.FindProperty("RoomCoordinates");
     }
 
-    public void OnSceneGUI()
-    {
-        SerializedProperty property = serializedObject.GetIterator();
-        while (property.Next(true))
-        {
-            if (property.propertyType == SerializedPropertyType.Vector3)
-            {
-                handleVectorProperty(property);
-            }
-            else
-            {
-                for (int x = 0; x < _roomCoordinates.arraySize; x++)
-                {
-                    SerializedProperty element = _roomCoordinates.GetArrayElementAtIndex(x);
-                    SerializedProperty _roomCoord = element.FindPropertyRelative("coordinate");
-                    SerializedProperty _roomName = element.FindPropertyRelative("name");
-                    handleVectorPropertyInArray(_roomCoord, _roomCoordinates, _roomName.stringValue);
-                }
-            }
-        }
+    //public void OnSceneGUI()
+    //{
+    //    SerializedProperty property = serializedObject.GetIterator();
+    //    while (property.Next(true))
+    //    {
+    //        if (property.propertyType == SerializedPropertyType.Vector3)
+    //        {
+    //            handleVectorProperty(property);
+    //        }
+    //        else
+    //        {
+    //            for (int x = 0; x < _roomCoordinates.arraySize; x++)
+    //            {
+    //                SerializedProperty element = _roomCoordinates.GetArrayElementAtIndex(x);
+    //                SerializedProperty _roomCoord = element.FindPropertyRelative("coordinate");
+    //                SerializedProperty _roomName = element.FindPropertyRelative("name");
+    //                handleVectorPropertyInArray(_roomCoord, _roomCoordinates, _roomName.stringValue);
+    //            }
+    //        }
+    //    }
 
-    }
+    //}
 
-    void handleVectorProperty(SerializedProperty property)
-    {
-        FieldInfo field = serializedObject.targetObject.GetType().GetField(property.name);
-        if (field == null)
-        {
-            return;
-        }
-        Handles.Label(property.vector3Value, property.name);
-        property.vector3Value = Handles.PositionHandle(property.vector3Value, Quaternion.identity);
-        serializedObject.ApplyModifiedProperties();
-    }
+    //void handleVectorProperty(SerializedProperty property)
+    //{
+    //    FieldInfo field = serializedObject.targetObject.GetType().GetField(property.name);
+    //    if (field == null)
+    //    {
+    //        return;
+    //    }
+    //    Handles.Label(property.vector3Value, property.name);
+    //    property.vector3Value = Handles.PositionHandle(property.vector3Value, Quaternion.identity);
+    //    serializedObject.ApplyModifiedProperties();
+    //}
 
-    void handleVectorPropertyInArray(SerializedProperty property, SerializedProperty parent, string name)
-    {
-        FieldInfo parentfield = serializedObject.targetObject.GetType().GetField(parent.name);
-        if (parentfield == null)
-        {
-            return;
-        }
-        Handles.Label(property.vector3Value + ((MonoBehaviour)target).transform.position, parent.name + "[" + name + "]");
-        property.vector3Value = Handles.PositionHandle(property.vector3Value + ((MonoBehaviour)target).transform.position, Quaternion.identity) - ((MonoBehaviour)target).transform.position;
-        serializedObject.ApplyModifiedProperties();
-    }
+    //void handleVectorPropertyInArray(SerializedProperty property, SerializedProperty parent, string name)
+    //{
+    //    FieldInfo parentfield = serializedObject.targetObject.GetType().GetField(parent.name);
+    //    if (parentfield == null)
+    //    {
+    //        return;
+    //    }
+    //    Handles.Label(property.vector3Value + ((MonoBehaviour)target).transform.position, parent.name + "[" + name + "]");
+    //    property.vector3Value = Handles.PositionHandle(property.vector3Value + ((MonoBehaviour)target).transform.position, Quaternion.identity) - ((MonoBehaviour)target).transform.position;
+    //    serializedObject.ApplyModifiedProperties();
+    //}
 }
 #endif
