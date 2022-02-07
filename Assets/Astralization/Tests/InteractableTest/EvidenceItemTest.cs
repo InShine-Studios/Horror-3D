672bba827f1ffe42bb45aae70441c5a135d1c562
Jahns Michael <michael.jf.jm@gmail.com>
2022-02-07 14:31:08 +0700

    add test for EvidenceItem and Thermometer

---
+++ Assets/Astralization/Tests/InteractableTest/EvidenceItemTest.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+public class EvidenceItemTest : TestBase
+{
+    #region Setup Teardown
+    [SetUp]
+    public override void SetUp()
+    {
+        sceneName = "EvidenceItemTestScene";
+        base.SetUp();
+    }
+    #endregion
+
+    #region Discard When Used
+    [UnityTest]
+    public IEnumerator EvidenceItem_DiscardWhenUsedEvidenceItem()
+    {
+        yield return new WaitWhile(() => sceneLoaded == false);
+        GameObject thermometer = GameObject.Find("Thermometer");
+        float moveDuration = GetMovementDurationTowards(thermometer.transform);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.MoveBack, false, moveDuration);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.PickItem);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.UseItem);
+        GameObject overworldThermometer = GameObject.Find("OverworldItems/Thermometer");
+        Image img = hud.transform.Find("ItemHud/Logo").GetComponent<Image>();
+        IInventory inventory = player.transform.Find("Rotate/InteractZone").GetComponent<IInventory>();
+        Assert.NotNull(overworldThermometer);
+        Assert.IsFalse(img.enabled);
+        Assert.AreEqual(0, inventory.GetNumOfItem());
+        Assert.IsNull(inventory.GetActiveItem());
+    }
+    #endregion
+
+    #region  Activated and Not Activated
+    [UnityTest]
+    public IEnumerator EvidenceItem_ActivatedWhenUsed()
+    {
+        yield return new WaitWhile(() => sceneLoaded == false);
+        GameObject thermometer = GameObject.Find("Thermometer");
+        float moveDuration = GetMovementDurationTowards(thermometer.transform);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.MoveBack, false, moveDuration);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.PickItem);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.UseItem);
+        GameObject thermometerModel = GameObject.Find("OverworldItems/Thermometer/Model");
+        Material stateMaterial = thermometerModel.GetComponent<MeshRenderer>().material;
+        Assert.AreEqual("MAT_Thermometer_Active (Instance)", stateMaterial.name);
+    }    
+    
+    [UnityTest]
+    public IEnumerator EvidenceItem_NotActivatedWhenUsed()
+    {
+        yield return new WaitWhile(() => sceneLoaded == false);
+        GameObject thermometer = GameObject.Find("Thermometer");
+        float moveDuration = GetMovementDurationTowards(thermometer.transform);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.MoveBack, false, moveDuration);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.PickItem);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.DiscardItem);
+        GameObject thermometerModel = GameObject.Find("OverworldItems/Thermometer/Model");
+        Material stateMaterial = thermometerModel.GetComponent<MeshRenderer>().material;
+        Assert.AreEqual("MAT_Thermometer_Base (Instance)", stateMaterial.name);
+    }    
+    #endregion
+    
+    #region Ghost Interaction Simulation
+
+    // [TODO] Adjust/remove when Ghost Interaction fully implemented
+    [UnityTest]
+    public IEnumerator EvidenceItem_SimulateGhostInteractionWhenNotActivated()
+    {
+        yield return new WaitWhile(() => sceneLoaded == false);
+        GameObject thermometer = GameObject.Find("Thermometer");
+        float moveDuration = GetMovementDurationTowards(thermometer.transform);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.MoveBack, false, moveDuration);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.PickItem);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.DiscardItem);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.SimulateGhostInteract);
+        GameObject thermometerModel = GameObject.Find("OverworldItems/Thermometer/Model");
+        Material stateMaterial = thermometerModel.GetComponent<MeshRenderer>().material;
+        Assert.AreEqual("MAT_Thermometer_Base (Instance)", stateMaterial.name);
+    }      
+    
+    [UnityTest]
+    public IEnumerator EvidenceItem_SimulateGhostInteractionPositive()
+    {
+        yield return new WaitWhile(() => sceneLoaded == false);
+        GameObject thermometer = GameObject.Find("Thermometer");
+        float moveDuration = GetMovementDurationTowards(thermometer.transform);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.MoveBack, false, moveDuration);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.PickItem);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.UseItem);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.SimulateGhostInteract);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.SimulateGhostInteract);
+        GameObject thermometerModel = GameObject.Find("OverworldItems/Thermometer/Model");
+        Material stateMaterial = thermometerModel.GetComponent<MeshRenderer>().material;
+        Assert.AreEqual("MAT_Thermometer_Positive (Instance)", stateMaterial.name);
+    }    
+    
+    [UnityTest]
+    public IEnumerator EvidenceItem_SimulateGhostInteractionNegative()
+    {
+        yield return new WaitWhile(() => sceneLoaded == false);
+        GameObject thermometer = GameObject.Find("Thermometer");
+        float moveDuration = GetMovementDurationTowards(thermometer.transform);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.MoveBack, false, moveDuration);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.PickItem);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.UseItem);
+        yield return SimulateInput(KeyboardMouseTestFixture.RegisteredInput.SimulateGhostInteract);
+        GameObject thermometerModel = GameObject.Find("OverworldItems/Thermometer/Model");
+        Material stateMaterial = thermometerModel.GetComponent<MeshRenderer>().material;
+        Assert.AreEqual("MAT_Thermometer_Negative (Instance)", stateMaterial.name);
+    }
+
+    #endregion
+}
