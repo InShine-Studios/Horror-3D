76d2d466968093fc83297abb29522e9f3d5371d6
Rafif Taris <51603162+rafiftaris@users.noreply.github.com>
2022-02-10 12:15:23 +0700

    [refactor] testing part 2 (#186)
    * Extract game object finding to subclasses
    
    * refactor test scene and find gameobjects

--- Assets/Astralization/Tests/GhostTest/GhostTest.cs
+++ Assets/Astralization/Tests/GhostTest/GhostTest.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 public class GhostTest : TestBase
 {
+    private GameObject ghost;
+    private IGhostMovement ghostMovement;
+
+    protected override void FindGameObjects(Scene scene)
+    {
+        GameObject[] gameObjects = scene.GetRootGameObjects();
+        foreach (GameObject gameObject in gameObjects)
+        {
+            if (gameObject.name == "Ghost")
+            {
+                ghost = gameObject;
+                ghostMovement = ghost.GetComponent<IGhostMovement>();
+            }
+        }
+    }
+
     #region Setup Teardown
     [SetUp]
     public override void SetUp()
@@ -19,8 +36,6 @@ public class GhostTest : TestBase
     public IEnumerator Ghost_WanderToTargetRoom()
     {
         yield return new WaitWhile(() => sceneLoaded == false);
-        GameObject ghost = GameObject.Find("Ghost");
-        IGhostMovement ghostMovement = ghost.GetComponent<IGhostMovement>();
 
         string targetRoomName = "Laundry Room";
         RoomCoordinate targetRoom = StageController.GetRoomCoordinate(targetRoomName);
@@ -41,8 +56,6 @@ public class GhostTest : TestBase
     public IEnumerator Ghost_WanderingRandomly()
     {
         yield return new WaitWhile(() => sceneLoaded == false);
-        GameObject ghost = GameObject.Find("Ghost");
-        IGhostMovement ghostMovement = ghost.GetComponent<IGhostMovement>();
         Vector3 initialPosition = ghost.transform.position;
 
         yield return new WaitWhile(ghostMovement.IsOnRoute);
