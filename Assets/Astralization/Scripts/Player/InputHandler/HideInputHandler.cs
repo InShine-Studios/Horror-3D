59a4abbe24955f1d6bf22967764c788909918622
naufalhilmi12345 <44135006+naufalhilmi12345@users.noreply.github.com>
2022-02-15 23:29:15 +0700

    [feature] hide mechanic (#192)
    * Adding hide mechanic
    
    * adding new impement of hiding
    
    * changing new logic for temp hiding
    
    * adding test
    
    * refactor some code

---
+++ Assets/Astralization/Scripts/Player/InputHandler/HideInputHandler.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/*
+ * Class to accept player input and continue with the dialogue
+ */
+public class HideInputHandler : MonoBehaviour
+{
+    #region Variable
+    public static event Action<bool> StopHidingHudEvent;
+    public static event Action<string> StopHidingEvent;
+
+    private string _playerActionMap = "Player";
+    #endregion
+
+    public void UnhidePlayer(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            StopHidingEvent?.Invoke(_playerActionMap);
+            StopHidingHudEvent?.Invoke(false);
+        }
+    }
+}
