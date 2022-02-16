07cc1a12bc7ada2fa010ef7665841d6a5c8700ba
uci <sef.gustingurah.yama@gmail.com>
2022-02-16 16:29:51 +0700

    ketinggalan

--- Assets/Astralization/Scripts/_Common/HidingState.cs
+++ Assets/Astralization/Scripts/_Common/HidingState.cs
@@ -1,22 +1,17 @@
 using UnityEngine.InputSystem;
 
-public class DialogueStaet : PlayerState
+public class HidingState : PlayerState
 {
     #region Movement Variables
-    private DialogueInputHandler _dialogueInputHandler;
     #endregion
 
     public override void Enter()
     {
         base.Enter();
-        _dialogueInputHandler = GetComponent<DialogueInputHandler>();
     }
 
     protected override void HandleInput(InputAction input, InputAction.CallbackContext ctx)
     {
-        switch (input.name)
-        {
-            case "NextDialogue": _dialogueInputHandler.NextDialogue(ctx); break;
-        }
+
     }
 }
