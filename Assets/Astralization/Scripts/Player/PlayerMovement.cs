a2cc3657a6b920f9da15e4d52468b9ee06dd05de
Rafif Taris <rafiftaris1@gmail.com>
2022-03-09 10:44:52 +0700

    WIP: rework camera

--- Assets/Astralization/Scripts/Player/PlayerMovement.cs
+++ Assets/Astralization/Scripts/Player/PlayerMovement.cs
@@ -29,19 +29,29 @@ public class PlayerMovement : MonoBehaviour, IPlayerMovement
     private CharacterController _controller;
     [SerializeField] [Tooltip("The PlayerBase for constants")]
     private PlayerBase _playerBase;
+    [SerializeField] [Tooltip("The camera that follows the player")]
+    private Camera _mainCamera;
 
     [Space]
     [Header("Movement Constants")]
     [Tooltip("The movement speed used")]
     private float _curMoveSpeed;
-    [Tooltip("The move direction generated")]
+    [Tooltip("The facing direction generated from keyboard")]
+    private Vector3 _faceDirection = new Vector3(0, 0, 0);
+    [Tooltip("The move angle calculated relative to camera position")]
+    private float _moveAngle;
+    [Tooltip("The move direction calculated from move quaternion")]
     private Vector3 _moveDirection;
+    [SerializeField] [Tooltip("Smoothing duration for turning")]
+    private float turnSmoothTime = 0.1f;
+    [Tooltip("Smoothing velocity for turning")]
+    private float turnSmoothVelocity;
     [Tooltip("Gravity Strength")]
     public float Gravity = -50.0f;
     [Tooltip("Force Grounding Flag")] [SerializeField]
     private bool _useForceGrounding = true;
 
-    [Tooltip("Sprinting boolean")]
+    [Tooltip("True if player is sprinting")]
     private bool _isSprinting;
     #endregion
 
@@ -61,15 +71,29 @@ public class PlayerMovement : MonoBehaviour, IPlayerMovement
 
     private void FixedUpdate()
     {
+        SetDirection();
         if (MovePlayer()) FindClosest?.Invoke();
         if (_useForceGrounding) ForceGrounding();
     }
     #endregion
 
     #region Handler
+    private void SetDirection()
+    {
+        Vector3 direction = new Vector3(
+            transform.position.x - _mainCamera.transform.position.x,
+            0f,
+            transform.position.z - _mainCamera.transform.position.z
+        ).normalized;
+        _moveAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, _moveAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+    }
+
     public void GenerateMoveVector(Vector2 moveInput)
     {
-        _moveDirection = new Vector3(0, 0) { x = moveInput.x, z = moveInput.y };
+        _faceDirection = new Vector3(0, 0) { x = moveInput.x, z = moveInput.y }.normalized;
+        _moveDirection = Quaternion.Euler(0f, _moveAngle, 0f) * _faceDirection;
         //Debug.Log("[PLAYER MOVEMENT] Direction: " + _moveDirection);
     }
 
@@ -83,10 +107,13 @@ public class PlayerMovement : MonoBehaviour, IPlayerMovement
 
     private bool MovePlayer()
     {
-        if (_isSprinting) _curMoveSpeed = _playerBase.GetSprintSpeed();
-        else _curMoveSpeed = _playerBase.GetPlayerMovementSpeed();
-        _controller.SimpleMove(_curMoveSpeed * Time.deltaTime * _moveDirection);
-        return _moveDirection.magnitude != 0;
+        if (_faceDirection.magnitude >= 0.1f)
+        {
+            if (_isSprinting) _curMoveSpeed = _playerBase.GetSprintSpeed();
+            else _curMoveSpeed = _playerBase.GetPlayerMovementSpeed();
+            _controller.SimpleMove(_curMoveSpeed * Time.deltaTime * _moveDirection.normalized);
+        }
+        return _faceDirection.magnitude != 0;
     }
     #endregion
 }
