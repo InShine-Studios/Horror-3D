3e2f1bd919bede9dfe4bcb344cd1fa52e71fbd49
Rafif Taris <rafiftaris1@gmail.com>
2022-03-16 12:19:02 +0700

    fix door controller

--- Assets/Astralization/Scripts/Interactables/Controller/DoorController.cs
+++ Assets/Astralization/Scripts/Interactables/Controller/DoorController.cs
@@ -15,6 +15,7 @@ public class DoorController : Interactable, IDoorController
 {
     #region Constants
     private const string _animParam = "isOpen";
+    private static readonly string[] _transitionAnimNames = { "Opening", "Closing" };
     #endregion
 
     #region Variables
@@ -23,9 +24,6 @@ public class DoorController : Interactable, IDoorController
     [Tooltip("True if door is in open state")]
     private bool _isOpen = false;
 
-    [Space]
-    [SerializeField]
-    [Header("Animation")]
     private Animator _animator;
     #endregion
 
@@ -43,9 +41,24 @@ public class DoorController : Interactable, IDoorController
     // General function to change the state of doors
     private void ChangeState()
     {
-        //Debug.Log("[INTERACTABLE] " + (isOpen ? "Closing " : "Opening ") + this.name);
-        _isOpen = !_isOpen;
-        _animator.SetBool(_animParam, _isOpen);
+        if (!IsTransitioning())
+        {
+            //Debug.Log("[INTERACTABLE] " + (isOpen ? "Closing " : "Opening ") + this.name);
+            _isOpen = !_isOpen;
+            _animator.SetBool(_animParam, _isOpen);
+            if (_isOpen) PlayAudio("Door_Open");
+            else PlayAudio("Door_Close");
+        }
+    }
+
+    public bool IsTransitioning()
+    {
+        bool isTransitioning = false;
+        foreach (string transitionAnimName in _transitionAnimNames)
+        {
+            isTransitioning |= _animator.GetCurrentAnimatorStateInfo(0).IsName(transitionAnimName);
+        }
+        return isTransitioning;
     }
     #endregion
 
@@ -53,6 +66,7 @@ public class DoorController : Interactable, IDoorController
     protected override void Awake()
     {
         base.Awake();
+        _animator = GetComponentInParent<Animator>();
     }
     #endregion
 
@@ -60,8 +74,6 @@ public class DoorController : Interactable, IDoorController
     public override void OnInteraction()
     {
         ChangeState();
-        if (_isOpen) PlayAudio("Door_Open");
-        else PlayAudio("Door_Close");
     }
     #endregion
 
